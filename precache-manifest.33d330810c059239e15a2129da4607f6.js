a0d654dc49730ae266ff4b1752895f4c18eb2735
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2020-07-18 15:48:38 +0530

    Updates

---
+++ precache-manifest.33d330810c059239e15a2129da4607f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "136334fa238f2029f7d4501a252b32a9",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "42e44441c743becc6aa2",
+    "url": "/first-react-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5880a679c1ee97aef237",
+    "url": "/first-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "42e44441c743becc6aa2",
+    "url": "/first-react-app/static/js/2.2d87815c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/first-react-app/static/js/2.2d87815c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5880a679c1ee97aef237",
+    "url": "/first-react-app/static/js/main.b9d4eb3d.chunk.js"
+  },
+  {
+    "revision": "cfbac3d06a28d0385198",
+    "url": "/first-react-app/static/js/runtime-main.6219d92b.js"
+  }
+]);
