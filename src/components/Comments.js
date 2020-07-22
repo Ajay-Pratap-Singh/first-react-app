5335af75f69e58e9d3d2fcd1f84edce261ca6443
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2020-07-22 11:19:02 +0530

    responsive fonts may be

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,15 +1,8 @@
-import React,{useState, createContext} from 'react';
-import AuthorInfo from './AuthorInfo';
-import ActionBar from './ActionBar';
+import React,{useState} from 'react';
 import Entity from './Entity';
 import {FaComment,FaAngleDown,FaAngleUp} from 'react-icons/fa';
 import {Button,Card,ListGroup,InputGroup,FormControl,Col,Row,Collapse, Container} from 'react-bootstrap';
 import axios from 'axios';
-
-const VoteContext=createContext({
-    voted:null,
-    setVoted:()=>{}
-});
 
 function CommentIcon(props){
     return (
@@ -36,23 +29,6 @@ function CommentIcon(props){
     );
 }
 
-function Comment(props) {
-    const [voted,setVoted]=useState(null);//use props false(dislike)  null(not-voted)  true(like)
-    const voteValue={voted,setVoted};
-    return (
-        <Card bg="light" className="w-100 border-bottom-0 border-left-0 border-right-0 rounded-0">
-            <Card.Body>
-                <AuthorInfo min/>
-                {props.body}
-                <VoteContext.Provider value={voteValue}>
-                    <ActionBar vote report/>
-                </VoteContext.Provider>
-            </Card.Body>
-            <CommentSection commentCount={2} parentId={2} parentType="comment"/>
-        </Card>
-    );
-}
-
 function CommentSection(props) {
     const [open, setOpen] = useState(false);
     const [comments, setComments] = useState([]);
@@ -60,8 +36,10 @@ function CommentSection(props) {
         return null;
     const showComments = async ()=>{
         setOpen(!open)
-        console.log('Fetching Comments for',props.parentId);
-        setComments((await axios.get('https://jsonplaceholder.typicode.com/comments?_limit=2')).data);
+        if(comments.length===0){
+            console.log('Fetching Comments for',props.parentId);
+            setComments((await axios.get('https://jsonplaceholder.typicode.com/comments?_limit=2')).data);
+        }
     }
     return (
         <Card.Footer className="p-0 pl-3">
@@ -86,9 +64,16 @@ function CommentSection(props) {
                 <Row>
                     <Collapse in={open}>
                         <ListGroup id="comment-sec" variant="flush" className="border-left">
-                            {comments.map((obj)=><ListGroup.Item className="p-0 border-0"><Entity
-                            bg="light" className="w-100 border-bottom-0 border-left-0 border-right-0 rounded-0"
-                             type="comment" body={obj.body}/></ListGroup.Item>)}
+                            {comments.map((obj,key)=>
+                                <ListGroup.Item key={key} className="p-0 border-0">
+                                    <Entity
+                                        bg="light" 
+                                        className="w-100 border-bottom-0 border-left-0 border-right-0 rounded-0"
+                                        type="comment" 
+                                        body={obj.body}
+                                    />
+                                </ListGroup.Item>)
+                            }
                         </ListGroup>
                     </Collapse>
                 </Row>
