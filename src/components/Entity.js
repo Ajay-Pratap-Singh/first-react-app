ef26c0ed90a7b782722c38325da815bae9429716
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2020-08-06 17:34:19 +0530

    show more implemented

--- src/components/Entity.js
+++ src/components/Entity.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useRef } from 'react';
 import {Card} from 'react-bootstrap';
 import AuthorInfo from './AuthorInfo';
 import {CommentSection,CommentInput} from './Comments';
@@ -13,6 +13,8 @@ const VoteContext=createContext({
 
 const Entity = (props) => {
     const [openComments, setOpenComments] = useState(false);
+    const [expanded, setExpanded] = useState(false);
+    const bodyContent = useRef(null);
     const [comments, setComments] = useState([]);
     const [voted,setVoted]=useState(null);//use props false(dislike)  null(not-voted)  true(like)
     const voteValue={voted,setVoted};
@@ -21,20 +23,22 @@ const Entity = (props) => {
             <Card.Body className="p-2 p-sm-3">
                 {props.type!=="comment"?<EntityHeader min type={props.type} eid={props.id} title={props.title}/>:null}
                 <AuthorInfo min={props.type==='comment'}/>
-                <ShowMoreText
-                    lines={3}
-                    more='Show more'
-                    less=''
-                    anchorClass=''
-                    expanded={false}
+                <div ref={bodyContent} style={{maxHeight:expanded?"none":"11ch",overflow:"hidden"}}>
+                    {props.body}
+                </div>
+                <p style={{textAlign:"right", color:"blue",cursor:"pointer"}} onClick={(e)=>{
+                    setExpanded(true);
+                    e.target.style.visibility="hidden"
+                    console.log("Show more clicked",bodyContent)
+                    }}
                 >
-                    {props.body+props.body+props.body}
-                    <VoteContext.Provider value={voteValue}>
-                        <ActionBar views={props.type!=='comment'} vote comment share={props.type!=='comment'} report setOpenComments={setOpenComments} openComments={openComments} />
-                    </VoteContext.Provider>
-                    {openComments?<CommentInput setOpenCommentInput={setOpenComments} comments={comments} 
-                    setComments={setComments}/>:null}
-                </ShowMoreText>
+                    Show more
+                </p>
+                <VoteContext.Provider value={voteValue}>
+                    <ActionBar views={props.type!=='comment'} vote comment share={props.type!=='comment'} report setOpenComments={setOpenComments} openComments={openComments} />
+                </VoteContext.Provider>
+                {openComments?<CommentInput setOpenCommentInput={setOpenComments} comments={comments} 
+                setComments={setComments}/>:null}
             </Card.Body>
             {openComments?<CommentSection comments={comments} setComments={setComments} parentType={props.type} parentId={props.id} commentCount={3}/>:null}
         </Card>
